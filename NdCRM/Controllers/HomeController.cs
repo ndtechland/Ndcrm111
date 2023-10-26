9bcfa770101484a5a8fb69d5cd4aa3003ac7f48f
123akanksha <ndinfotechteam@gmail.com>
2023-10-26 12:32:13 +0530

    changesakanksha

--- NdCRM/Controllers/HomeController.cs
+++ NdCRM/Controllers/HomeController.cs
@@ -31,7 +31,10 @@ namespace NdCRM.Controllers
         {
             return View();
         }
-
+        public ActionResult Login()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
